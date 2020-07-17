7736679b367dbde901534b533077eec7b3beb595
marzeu <marzahell@hotmail.com>
2020-07-16 23:06:26 -0300

    detalhes

--- main.js
+++ main.js
@@ -1,40 +1,68 @@
-// função que lista professores////
-
-var teacher = {
-  id: ["teacher"],
-  name: ["Lara", "Marzeu", "Almir", "Diogo", "Sheila"],
-  classes: ["Francês", "Física", "Matemática", "Biologia", "Biologia"]
+// Objeto de alunos
+var student = {
+  id: "students",
+  students: [{ name: "Alice", status: "Pago", turma: "101", teacher: "Marcos"}]
 };
 
+//Objeto de professores
+var teacher = {
+  id: "teacher",
+  teachers: [{ name: "Lara", classes: "Francês" },
+  { name: "Marzeu", classes: "Física" },
+  { name: "Almir", classes: "Matemática" },
+  { name: "Diogo", classes: "Biologia" },
+  { name: "Sheila", classes: "Biologia" }
+  ]
+};
 
 // Função responsável por criar a tabela dinamina tanto para alunos quanto para professores
-function createTable(tableName) {
-  let table = document.getElementById(tableName.id);
+
+//função que lista alunos, pagamentos e turma//
+
+function listTeacher() {
+  let table = document.getElementById('teacher');
+  let tbody = document.createElement('tbody'); 
+ for (let j in teacher.teachers) {
+  let createRow = document.createElement('tr');
+  let firstData = document.createElement('td')
+  firstData.style.textAlign = "center";
+  let secondData = document.createElement('td');
+  secondData.style.textAlign = "center";
+  firstData.innerHTML = `${teacher.teachers[j].name}`;
+  secondData.innerHTML = `${teacher.teachers[j].classes}`;
+  createRow.appendChild(firstData);
+  createRow.appendChild(secondData);
+  tbody.appendChild(createRow);
+  table.appendChild(tbody);
+ };
+};
+
+function listStudent() {
+  let table = document.getElementById('student');
   let tbody = document.createElement('tbody');
-  let numOfRows = tableName[Object.keys(tableName)[1]];
-  let numOfCols = tableName[Object.keys(tableName)[2]];
-
-  for (let j in numOfRows) {
-    let createRow = document.createElement('tr');
-    let firstData = document.createElement('td')
-    firstData.style.textAlign = "center";
-    let secondData = document.createElement('td');
-    secondData.style.textAlign = "center";
-    firstData.innerHTML = `${numOfRows[j]}`;
-    secondData.innerHTML = `${numOfCols[j]}`;
-    createRow.appendChild(firstData);
-    createRow.appendChild(secondData);
-    tbody.appendChild(createRow);
-    table.appendChild(tbody);
-  };
+ for (let j in student.students) {
+  let createRow = document.createElement('tr');
+  let firstData = document.createElement('td');
+  firstData.style.textAlign = "center";
+  let secondData = document.createElement('td');
+  secondData.style.textAlign = "center";
+  let thirdData = document.createElement('td');
+  thirdData.style.textAlign = "center";
+  let fourthData = document.createElement('td');
+  fourthData.style.textAlign = "center";
+  firstData.innerHTML = `${student.students[j].name}`; secondData.innerHTML = `${student.students[j].status}`;
+  thirdData.innerHTML = `${student.students[j].turma}`;
+  fourthData.innerHTML = `${student.students[j].teacher}`;
+  createRow.appendChild(firstData);
+  createRow.appendChild(secondData);
+  createRow.appendChild(thirdData);
+  createRow.appendChild(fourthData);
+  tbody.appendChild(createRow);
+  table.appendChild(tbody);
+ };
 };
 
-
-//função que lista alunos e pagamentos//
-
-var students = {
-  id: ["students"],
-  name: ["Alice", "Breno", "Caio", "Luísa", "Paulo"],
-  status: ["Pago", "Pendente", "Dívida", "Pago", "Pendente"],
-};
-
+function add(){  
+ let classeName = document.getElementById('turmaid').value;
+ console.log(classeName);
+}
